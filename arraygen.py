e44c13febcf16882b3e58c269cc459ede78a9599
JasonZhaoExp <jasonzhaoexp@gmail.com>
2023-07-12 12:33:23 +0100

    updated arraygen.py

--- arraygen.py
+++ arraygen.py
@@ -2,13 +2,15 @@
 
 from Crypto.Random import random
 
+def generate_random_sequenced_array(min_num=0, max_num=1024):
+    array = [a for a in range(min_num, max_num)]
+    return array_randomizer(array)
+
 def generate_random_array(size=1024, min_num=0, max_num=1024):
     array = []
-    for i in range (size):
+    for i in range(size):
         array.append(random.randint(min_num, max_num))
     return array
 
 def array_randomizer(array = [a for a in range(1, 1024)]):
-    array_copy = array
-    for i in range(len(array)):
-        array[i] = random.choice(array_copy)
+    return random.shuffle(array)
